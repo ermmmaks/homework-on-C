8c407fd3ed4ae43dd1c9a84364fa0bc2af8e1015
ermmmaks <maks.ermoshkin56@gmail.com>
2025-12-19 15:57:09 +0300

    Delete unused files

--- cycleLists.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <stdbool.h>
-
-List* new(void);
-bool insert(list* list, int index int value);
-int get(list* list);
-bool remove(List* list, int index);
-void printList(List* list);
-void deleteList(List* list);
