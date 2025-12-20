186f14e755aeef963124d4d6471d415e84b1bb3d
ermmmaks <maks.ermoshkin56@gmail.com>
2025-12-21 02:32:32 +0300

    Forgot about saving main file. Now compilation is done

--- 6.rhyme/rhyme.c
+++ 6.rhyme/rhyme.c
@@ -1,33 +1,6 @@
 #include "cycleLists.h"
 #include <stdio.h>
 
-int trick(int n, int m)
-{
-    if (n <= 0 || m <= 0) {
-        return -1;
-    }
-
-    CycleList* list = createNew();
-    for (int i = 1; i < n + 1; i++) {
-        if (!insert(list, i - 1, i)) {
-            printf("Error adding\n");
-        }
-    }
-
-    ListNode* curr = list->tail;
-
-    while (list->tail->next != list->tail) {
-        for (int i = 0; i < m - 1; i++) {
-            curr = curr->next;
-        }
-        removeNext(list, curr);
-    }
-
-    int survivor = list->tail->value;
-    deleteList(list);
-    return survivor;
-}
-
 int main()
 {
     int warriors = 0;
